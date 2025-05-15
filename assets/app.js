dcd66f15b20065d168ac51aa53da4d5e37682525
Gustavo Silva Santiago <gustavox00.santi@outlook.com>
2025-05-15 16:08:53 -0300

    Finalização do projeto

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-onload = () =>{
+onload = () => {
     document.querySelector('#bt0').onclick = () => digito(0)
     document.querySelector('#bt1').onclick = () => digito(1)
     document.querySelector('#bt2').onclick = () => digito(2)
@@ -16,4 +16,74 @@ onload = () =>{
     document.querySelector('#btdiv').onclick = () => operador('/')
     document.querySelector('#btmulti').onclick = () => operador('*')
     document.querySelector('#btigual').onclick = () => calcula()
+}
+
+let sValor = '0'
+let eNovoNumero = true
+let valorAnterior = 0
+let operacaoPendente = null
+
+const atualizaVisor = () => {
+    let [parteInteira, parteDecimal] = sValor.split(',')
+    let v = ''
+    c = 0
+    for (let i = parteInteira.length - 1; i >= 0; i--) {
+        if (++c > 3) {
+            v = '.' + v
+            c = 1
+        }
+        v = parteInteira[i] + v
+    }
+    v = v + (parteDecimal ? ',' + parteDecimal : '')
+    document.querySelector('#visor').innerText = v;
+}
+
+const digito = (n) => {
+    if (eNovoNumero) {
+        sValor = '' + n;
+        eNovoNumero = false
+    } else sValor += n
+    atualizaVisor()
+}
+
+const virgula = () => {
+    if (eNovoNumero) {
+        sValor = '0,'
+        eNovoNumero = false
+    } else if (sValor.indexOf(',') == -1)
+        sValor += ','
+    atualizaVisor()
+}
+
+const limpa = () => {
+    eNovoNumero = true
+    sValor = '0'
+    valorAnterior = 0
+    operacaoPendente = null
+    atualizaVisor()
+}
+const valorAtual = () => parseFloat(sValor.replace(',', '.'))
+
+const operador = (op) => {
+    calcula()
+    valorAnterior = valorAtual()
+    operacaoPendente = op
+    eNovoNumero = true
+}
+
+const calcula = () => {
+    if (operacaoPendente != null) {
+        let resultado
+        switch (operacaoPendente) {
+            case '+': resultado = valorAnterior + valorAtual(); break;
+            case '-': resultado = valorAnterior - valorAtual(); break;
+            case '/': resultado = valorAnterior / valorAtual(); break;
+            case '*': resultado = valorAnterior * valorAtual(); break;
+        }
+        sValor = resultado.toString().replace('.', ',')
+    }
+    eNovoNumero = true
+    operacaoPendente = null
+    valorAnterior = 0
+    atualizaVisor()
 }
